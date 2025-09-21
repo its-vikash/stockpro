29109771cd24cdefeb211e27f50ed76c71afcbd2
Vikash Singh <vikash.cnn@gmail.com>
2025-09-21 06:20:18 +0530

    chnages for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/its-vikash/',
-});
+  base: './', // This is important for GitHub Pages
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+})
