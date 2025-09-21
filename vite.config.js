3f64179d1cbd4e4e81d503d2abc90a3b0e5cf2d7
Vikash Singh <vikash.cnn@gmail.com>
2025-09-21 14:11:49 +0530

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-  server: {
-    port: 3000,
-  }
+  base: '/stockpro/',
 })
