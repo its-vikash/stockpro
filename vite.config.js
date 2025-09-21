f13452eb4bd3accf1091f57185327085eda64cc7
Vikash Singh <vikash.cnn@gmail.com>
2025-09-21 06:31:07 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This is important for GitHub Pages
+  base: './', // Use relative paths
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
   }
 })
